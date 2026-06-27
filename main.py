a0ed3a64a82d5cc626b473fdd207937545074954
dilli1492001 <dilliprasad149@gmail.com>
2026-06-27 10:33:42 +0530

    add backend code

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi import WebSocket,WebSocketDisconnect
+import json
+
+connections=[]
+
+app=FastAPI()
+
+@app.websocket("/ws/{username}")
+async def websocket_endpoint(websocket:WebSocket,username:str):
+    await websocket.accept()
+
+    user_connection={"username":username,"websocket":websocket}
+    connections.append(user_connection)
+
+    try:
+        while True:
+            data=await websocket.receive_text()
+
+            for connection in connections:
+                await connection["websocket"].send_text(
+                    json.dumps({'sender':username,"message":data})
+                )
+    
+    except WebSocketDisconnect:
+        connections.remove(user_connection)
+
+        print(f"{username} disconnected")
